2e6f214954b16e20074bac55a9496cd79f9ea1a8
Luca Battistella <luca.battistella.geo@gmail.com>
2021-08-04 14:47:36 +0200

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,13 +1,11 @@
-CREATE OR REPLACE FUNCTION marine_ecosystems.api_seagrass_wdpa_statistic(
-    wdpaid text DEFAULT NULL::text)
-    RETURNS SETOF marine_ecosystems.seagrass_wdpa_protection_perc 
+CREATE OR REPLACE FUNCTION marine_ecosystems.api_seagrass_wdpa_statistics(
+	wdpaid text DEFAULT NULL::text)
+    RETURNS SETOF marine_ecosystems.seagrass_wdpa_protection_perc
     LANGUAGE 'plpgsql'
+
     COST 100
-    VOLATILE PARALLEL UNSAFE
+    VOLATILE 
     ROWS 1000
-
- 
-
 AS $BODY$
 BEGIN
 IF $1 is null THEN
@@ -18,12 +16,8 @@ END IF;
 END
 $BODY$;
 
- 
-
-ALTER FUNCTION marine_ecosystems.api_seagrass_wdpa_statistic(text)
+ALTER FUNCTION marine_ecosystems.api_seagrass_wdpa_statistics(text)
     OWNER TO biopama_api_user;
 
- 
-
-COMMENT ON FUNCTION marine_ecosystems.api_seagrass_wdpa_statistic(text)
-    IS 'Seagrass statistics at PA level';
+COMMENT ON FUNCTION marine_ecosystems.api_seagrass_wdpa_statistics (text)
+    IS 'test';
